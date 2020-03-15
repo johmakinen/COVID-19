a35fffee0dc5630d7804afed44550a2fa8e6aa1e
johmakinen <joh.makinen@gmail.com>
2020-03-15 11:54:47 +0200

    made code readable

--- data_module_covid.py
+++ data_module_covid.py
@@ -5,23 +5,14 @@ import requests
 
 def extract_counts(df):
 
-    df_ts = df[['date', 'id']]
     new_dates = pd.Series([x[:10]
                            for x in df['date'].values], name='new_dates')
-    new_dates_s = new_dates.str.split('-', expand=True)
-    new_dates_s = new_dates_s.rename(columns={0: 'year', 1: 'month', 2: 'day'})
+    new_dates = pd.to_datetime(new_dates)
 
-    df_ts = pd.concat([df_ts, new_dates_s], axis=1)
-    df_ts = df_ts.loc[:, "id":"day"].rename(columns={'id': 'count'})
-    df_ts = df_ts.groupby(['year', 'month', 'day']).count()
+    res = pd.concat([new_dates, pd.Series(
+        np.ones(len(new_dates)), name="count")], axis=1)
 
-    # print(df_ts)
-    covid1 = pd.Series(df_ts['count'].values, name='count')
-    covid2 = pd.Series(new_dates.unique(), name="date").sort_values()
-    covid = pd.concat([covid2.reset_index(), covid1], axis=1)
-    covid["date"] = pd.to_datetime(covid['date'])
-    covid = covid.set_index('date').drop('index', axis=1)
-    return covid
+    return res.groupby('new_dates').count()
 
 
 def get_data_from_HS():
